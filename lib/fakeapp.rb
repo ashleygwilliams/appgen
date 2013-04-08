43443031a3e5b05bfa75d45b52f8fe95ecc77ae0
ashley williams <heyashleyashley@gmail.com>
2013-04-07 21:55:13 -0400

    add more tile sets

--- lib/fakeapp.rb
+++ lib/fakeapp.rb
@@ -5,7 +5,8 @@ class FakeApp
 
   TILE_LAYERS = ["'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: 'OpenStreetMap'}",
                   "'http://otile{s}.mqcdn.com/tiles/1.0.0/{type}/{z}/{x}/{y}.png', {subdomains: '1234', type: 'osm', attribution: 'MapQuestOpen'}",
-                  '"http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png", {attribution: "OpenCycleMap"}'
+                  "'http://otile{s}.mqcdn.com/tiles/1.0.0/{type}/{z}/{x}/{y}.png', {subdomains: '1234', type: 'sat', attribution: 'MapQuestOpen'}",
+                  '"http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png", {attribution: "OpenCycleMap"}',
   ]
 
   def self.getRandomName
